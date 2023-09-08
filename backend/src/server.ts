bcdf80cdeaed745075dfcc8675a4c2046d6b8903
Alex Meyer <acomeyer@gmail.com>
2023-09-07 19:35:36 -0500

    add dockerfile and typescript for deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
